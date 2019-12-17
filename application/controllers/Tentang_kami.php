427e03917caab35308b70ba44793b8b929cbb8bc
Muhammad Ibnu Wardana <muhammadibnuwardana@gmail.com>
2019-12-17 16:05:24 +0700

    Fix Navbar

--- application/controllers/Tentang_kami.php
+++ application/controllers/Tentang_kami.php
@@ -1,14 +1,19 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
-class Tentang_kami extends CI_Controller {
+class Tentang_kami extends CI_Controller
+{
+    public function index()
+    {
+        $this->profil();
+    }
     public function profil()
     {
         $data['topic'] = "Tentang Kami";
         $data['page'] = "Profil Perusahaan";
         $this->load->view('template/header', $data);
-		$this->load->view('tentang/profil', $data);
-		$this->load->view('template/footer');
+        $this->load->view('tentang/profil', $data);
+        $this->load->view('template/footer');
     }
 
     public function sejarah()
@@ -16,7 +21,15 @@ class Tentang_kami extends CI_Controller {
         $data['topic'] = "Tentang Kami";
         $data['page'] = "Sejarah";
         $this->load->view('template/header', $data);
-		$this->load->view('tentang/profil');
-		$this->load->view('template/footer');
+        $this->load->view('tentang/profil');
+        $this->load->view('template/footer');
+    }
+    public function visi_misi()
+    {
+        $data['topic'] = "Tentang Kami";
+        $data['page'] = "Sejarah";
+        $this->load->view('template/header', $data);
+        $this->load->view('tentang/profil');
+        $this->load->view('template/footer');
     }
 }
--- application/views/template/menu.php
+++ application/views/template/menu.php
@@ -1,91 +1,86 @@
 <!-- Main Navigation Start -->
-    <nav class="navbar navbar-expand-lg bg-transparent">
-      <div class="container text-nowrap">
-        <div class="d-flex align-items-center w-100 col p-0">
-          <a class="navbar-brand rounded-bottom light-bg" href="<?= base_url();?>">
-            <img src="<?php echo base_url('assets/')?>images/bpr-logo1.png" alt="">
-          </a> 
-        </div>
+<nav class="navbar navbar-expand-md bg-transparent">
+  <div class="container text-nowrap">
+    <div class="d-flex align-items-center w-100 col p-0">
+      <a class="navbar-brand rounded-bottom light-bg" href="<?= base_url(); ?>">
+        <img src="<?php echo base_url('assets/') ?>images/bpr-logo1.png" alt="">
+      </a>
+    </div>
 
-        <!-- Topbar Request Quote Start -->
-        <div class="d-inline-flex request-btn order-lg-last col p-0"> 
-         
-          <!-- Toggle Button Start -->
-          <button class="navbar-toggler x collapsed nav-link mr-2 ml-auto" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-        </button>
-          <!-- Toggle Button End -->  
-        </div>
-        <!-- Topbar Request Quote End -->
+    <!-- Toggle Button Start -->
+    <button class="navbar-toggler x collapsed nav-link mr-2 ml-auto" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="icon-bar"></span>
+      <span class="icon-bar"></span>
+      <span class="icon-bar"></span>
+    </button>
+    <!-- Toggle Button End -->
 
-        <div class="collapse navbar-collapse" id="navbarCollapse" data-hover="dropdown" data-animations="slideInUp slideInUp slideInUp slideInUp">
+    <div class="collapse navbar-collapse" id="navbarCollapse" data-hover="dropdown" data-animations="slideInUp slideInUp slideInUp slideInUp">
 
-          <ul class="navbar-nav ml-auto">
-              <li class="nav-item">
-                <a class="nav-link" href="<?= base_url();?>">Home</a>
-              </li>
-              <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle-mob" href="index.html" id="dropdown03" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Tentang Kami <i class="icofont-rounded-down"></i></a>                
-                <ul class="dropdown-menu" aria-labelledby="dropdown03">
-                  <li><a class="dropdown-item" href="<?= base_url('tentang_kami/profil');?>">Profil Perusahaan</a></li>
-                      <li><a class="dropdown-item" href="index-2.html">Sejarah</a></li>
-                      <li><a class="dropdown-item" href="index-3.html">Visi & Misi</a></li>
-                      <li><a class="dropdown-item" href="index-4.html">Board Manajemen</a></li>
-                      <li><a class="dropdown-item" href="index-5.html">Struktur Organisasi</a></li>
-                      <li><a class="dropdown-item" href="index-5.html">Penghargaan</a></li>
-                      <li><a class="dropdown-item" href="index-5.html">Lokasi Kantor</a></li>
-                </ul>
-              </li>
-              <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle-mob" href="" id="dropdown03" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Produk dan Layanan <i class="icofont-rounded-down"></i></a>                
-                <ul class="dropdown-menu" aria-labelledby="dropdown03">
-                  <li><a class="dropdown-item" href="index.html">Pembayaran Mahasiswa</a></li>
-                      <li><a class="dropdown-item" href="index-2.html">Online Payment</a></li>
-                      <li class="dropdown">
-                        <a class="dropdown-toggle-mob dropdown-item" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Deposito <i class="icofont-rounded-right float-right"></i></a>
-                      <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                        <li><a href="#" class="dropdown-item">Deposito Masyarakat</a></li>
-                        <li><a href="#" class="dropdown-item">Deposito Bank Umum</a></li>
-                      </ul>
-                      </li>
-                      <li class="dropdown">
-                      <a class="dropdown-toggle-mob dropdown-item" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Tabungan <i class="icofont-rounded-right float-right"></i></a>
-                      <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                        <li><a href="#" class="dropdown-item">TabunganKu</a></li>
-                        <li><a href="#" class="dropdown-item">Tabungan Sejahtera</a></li>
-                        <li><a href="#" class="dropdown-item">Tabungan Umroh</a></li>
-                        <li><a href="#" class="dropdown-item">Tabungan Qurban Warga</a></li>
-                      </ul>
-                  </li>
-                  <li class="dropdown">
-                      <a class="dropdown-toggle-mob dropdown-item" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Kredit <i class="icofont-rounded-right float-right"></i></a>
-                      <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                        <li><a href="#" class="dropdown-item">Kredit Bakulan</a></li>
-                        <li><a href="#" class="dropdown-item">Kredit Konsumtif</a></li>
-                        <li><a href="#" class="dropdown-item">Kredit Modal Kerja</a></li>
-                        <li><a href="#" class="dropdown-item">Kredit Investasi</a></li>
-                      </ul>
-                  </li>
-                </ul>
-              </li>
-              <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle-mob" href="index.html" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">News <i class="icofont-rounded-down"></i></a>
-                <ul class="dropdown-menu">                  
-                  <li><a class="dropdown-item" href="shortcodes-element.html">Artikel</a></li>
-                  <li><a class="dropdown-item" href="typography.html">Laporan Publikasi</a></li>                  
-                  <li><a class="dropdown-item" href="request-page.html">GCG Report</a></li>
-                  <li><a class="dropdown-item" href="request-page.html">Pengkinian Data Nasabah</a></li>
-                </ul>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link dropdown-toggle-mob" href="index.html" >Contact </a>
-              </li>
+      <ul class="navbar-nav ml-auto">
+        <li class="nav-item">
+          <a class="nav-link" href="<?= base_url(); ?>">Home</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle-mob" href="" id="dropdown03" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Tentang Kami <i class="icofont-rounded-down"></i></a>
+          <ul class="dropdown-menu" aria-labelledby="dropdown03">
+            <li><a class="dropdown-item" href="<?= base_url('tentang_kami/profil'); ?>">Profil Perusahaan</a></li>
+            <li><a class="dropdown-item" href="<?= base_url('tentang_kami/sejarah'); ?>">Sejarah</a></li>
+            <li><a class="dropdown-item" href="<?= base_url('tentang_kami/visi_misi'); ?>">Visi & Misi</a></li>
+            <li><a class="dropdown-item" href="<?= base_url('tentang_kami/manajemen'); ?>">Board Manajemen</a></li>
+            <li><a class="dropdown-item" href="<?= base_url('tentang_kami/struktur_organisasi'); ?>">Struktur Organisasi</a></li>
+            <li><a class="dropdown-item" href="<?= base_url('tentang_kami/achievement'); ?>">Penghargaan</a></li>
+            <li><a class="dropdown-item" href="<?= base_url('tentang_kami/location'); ?>">Lokasi Kantor</a></li>
           </ul>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle-mob" href="" id="dropdown03" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Produk dan Layanan <i class="icofont-rounded-down"></i></a>
+          <ul class="dropdown-menu" aria-labelledby="dropdown03">
+            <li><a class="dropdown-item" href="<?= base_url('produk_layanan/mahasiswa'); ?>">Pembayaran Mahasiswa</a></li>
+            <li><a class="dropdown-item" href="<?= base_url('produk_layanan/online_payment'); ?>">Online Payment</a></li>
+            <li class="dropdown">
+              <a class="dropdown-toggle-mob dropdown-item" href="" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Deposito <i class="icofont-rounded-right float-right"></i></a>
+              <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                <li><a href="<?= base_url('produk_layanan/deposito_masyarakat'); ?>" class="dropdown-item">Deposito Masyarakat</a></li>
+                <li><a href="<?= base_url('produk_layanan/deposito_aba'); ?>" class="dropdown-item">Deposito Bank Umum</a></li>
+              </ul>
+            </li>
+            <li class="dropdown">
+              <a class="dropdown-toggle-mob dropdown-item" href="" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Tabungan <i class="icofont-rounded-right float-right"></i></a>
+              <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                <li><a href="<?= base_url('produk_layanan/tabungan_ku'); ?>" class="dropdown-item">TabunganKu</a></li>
+                <li><a href="<?= base_url('produk_layanan/tabungan_sejahtera'); ?>" class="dropdown-item">Tabungan Sejahtera</a></li>
+                <li><a href="<?= base_url('produk_layanan/tabungan_umroh'); ?>" class="dropdown-item">Tabungan Umroh</a></li>
+                <li><a href="<?= base_url('produk_layanan/tabungan_qurban'); ?>" class="dropdown-item">Tabungan Qurban Warga</a></li>
+              </ul>
+            </li>
+            <li class="dropdown">
+              <a class="dropdown-toggle-mob dropdown-item" href="" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Kredit <i class="icofont-rounded-right float-right"></i></a>
+              <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                <li><a href="<?= base_url('produk_layanan/kredit_bakulan'); ?>" class="dropdown-item">Kredit Bakulan</a></li>
+                <li><a href="<?= base_url('produk_layanan/kredit_konsumtif'); ?>" class="dropdown-item">Kredit Konsumtif</a></li>
+                <li><a href="<?= base_url('produk_layanan/kredit_modal'); ?>" class="dropdown-item">Kredit Modal Kerja</a></li>
+                <li><a href="<?= base_url('produk_layanan/kredit_investasi'); ?>" class="dropdown-item">Kredit Investasi</a></li>
+              </ul>
+            </li>
+          </ul>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle-mob" href="" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">News <i class="icofont-rounded-down"></i></a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="<?= base_url('news/artikel'); ?>">Artikel</a></li>
+            <li><a class="dropdown-item" href="<?= base_url('news/publikasi'); ?>">Laporan Publikasi</a></li>
+            <li><a class="dropdown-item" href="<?= base_url('news/gcg'); ?>">GCG Report</a></li>
+            <li><a class="dropdown-item" href="<?= base_url('news/pengkinian_data'); ?>">Pengkinian Data Nasabah</a></li>
+          </ul>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link dropdown-toggle-mob" href="<?= base_url('contact'); ?>">Hubungi Kami </a>
+        </li>
+      </ul>
 
-          <!-- Main Navigation End -->
-        </div>
-      </div>
-    </nav>
-    <!-- Main Navigation End -->
+      <!-- Main Navigation End -->
+    </div>
+  </div>
+</nav>
+<!-- Main Navigation End -->
