e7a1ecdd17ddf8911d102c3ec946dc39daedf10d
Muhammad Ibnu Wardana <muhammadibnuwardana@gmail.com>
2020-03-16 10:24:37 +0700

    Commit

--- application/views/template/footer.php
+++ application/views/template/footer.php
@@ -104,7 +104,7 @@
 <script type="text/javascript" src="<?php echo base_url('assets/') ?>rev-slider/revolution/js/extensions/revolution.extension.navigation.min.js"></script>
 <script type="text/javascript" src="<?php echo base_url('assets/') ?>rev-slider/revolution/js/extensions/revolution.extension.parallax.min.js"></script>
 <script type="text/javascript" src="<?php echo base_url('assets/') ?>rev-slider/revolution/js/extensions/revolution.extension.slideanims.min.js"></script>
-<script type="text/javascript" src="<?php echo base_url('assets/') ?>rev-slider/revolution/js/extensions/revolution.extension.video.min.js"></script>
+<!-- <script type="text/javascript" src="<?php echo base_url('assets/') ?>rev-slider/revolution/js/extensions/revolution.extension.video.min.js"></script> -->
 
 <script src="<?php echo base_url('assets/') ?>js/site-custom.js"></script>
 
